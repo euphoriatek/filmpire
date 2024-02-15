5f2b3efaf7e65364e0cdd370209e343795154174
brijender <euphoriatek2021@gmail.com>
2024-02-15 19:30:21 +0530

    login

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const moviesApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: process.env.REACT_APP_TMDB_KEY,
+    api_key: '2e2cdc89526f48f50dbe44484d5668aa'
   },
 });
 
